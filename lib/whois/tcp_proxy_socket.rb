4633f4c3f3170f3dd86616ad223cb4e7571965f5
Justin Campbell <justin@justincampbell.me>
2012-08-30 13:36:12 -0400

    Use attr_accessors instead of instance variables
    ...and other minor cleanup

--- lib/whois/tcp_proxy_socket.rb
+++ lib/whois/tcp_proxy_socket.rb
@@ -3,26 +3,32 @@ require 'socket'
 
 module Whois
   class TCPProxySocket
-    attr_reader :tcp_socket
+    attr_accessor :host, :port, :proxy_options, :tcp_socket
 
     def initialize(*args)
-      @host = args.shift
-      @port = args.shift
-      @proxy_options = args.pop
-      @tcp_socket = TCPSocket.new(@proxy_options[:host], @proxy_options[:port], *args)
-      http_connect.each {|line| @tcp_socket.puts line; }
+      self.host = args.shift
+      self.port = args.shift
+      self.proxy_options = args.pop
+      self.tcp_socket = TCPSocket.new(proxy_options[:host], proxy_options[:port], *args)
+
+      http_connect.each do |line|
+        tcp_socket.puts line
+      end
     end
 
     private
 
     def basic_auth_header
-      "Proxy-Authorization: Basic " +
-        Base64.strict_encode64("#{@proxy_options[:user]}:#{@proxy_options[:pass]}")
+      "Proxy-Authorization: Basic #{encoded_credentials}"
+    end
+
+    def encoded_credentials
+      Base64.strict_encode64 "#{proxy_options[:user]}:#{proxy_options[:pass]}"
     end
 
     def http_connect
       [
-        "CONNECT #{@host}:#{@port} HTTP/1.1",
+        "CONNECT #{host}:#{port} HTTP/1.1",
         basic_auth_header,
         ""
       ]
